6fc7ae0ffd6611889541a415d5b45535ea920def
redaelemawy <redaelemawy98@gmail.com>
2026-09-13 10:14:38 +0300

    add text

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>React</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore,
+          consequatur deserunt impedit perferendis nobis, ullam error culpa
+          tempora voluptates dolor dicta quod. Labore quasi ex impedit
+          cupiditate? Animi, fuga inventore.
+        </p>
       </main>
     </div>
   );
