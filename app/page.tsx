877533cc9c6049daeb6f504dc9271ec225123d21
redaelemawy <redaelemawy98@gmail.com>
2026-09-13 12:12:03 +0300

    qwas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between text-green-700 py-32 px-16 sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between text-yellow-700 py-32 px-16 sm:items-start">
         <h1>React</h1>
         <hr />
         **********
